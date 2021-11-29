cd9fafc17d05ccecf15d324a8479ae77eb70b1e6
awenzelhuemer <43669899+Superandi1234@users.noreply.github.com>
2021-11-29 12:06:13 +0100

    Use of semaphores

--- exercise/Ue02/src/RaceConditions2/Program.cs
+++ exercise/Ue02/src/RaceConditions2/Program.cs
@@ -11,14 +11,13 @@ namespace RaceConditions2
             private const int BUFFER_SIZE = 10;
 
             private double[] buffer;
-            private AutoResetEvent writerReady;
-            private AutoResetEvent readerReady;
+            private Semaphore empty, full;
 
             public void Run()
             {
                 buffer = new double[BUFFER_SIZE];
-                writerReady = new AutoResetEvent(false);
-                readerReady = new AutoResetEvent(false);
+                empty = new Semaphore(BUFFER_SIZE, BUFFER_SIZE);
+                full = new Semaphore(0, BUFFER_SIZE);
 
                 // start threads 
                 var t1 = new Thread(Reader);
@@ -36,10 +35,13 @@ namespace RaceConditions2
                 var readerIndex = 0;
                 for (int i = 0; i < N; i++)
                 {
-                    writerReady.WaitOne();
-                    Console.WriteLine(buffer[readerIndex]);
-                    readerIndex = (readerIndex + 1) % BUFFER_SIZE;
-                    readerReady.Set();
+                    full.WaitOne(); // Wait until item available
+                    lock (buffer)
+                    {
+                        Console.WriteLine(buffer[readerIndex]);
+                        readerIndex = (readerIndex + 1) % BUFFER_SIZE;
+                    }
+                    empty.Release(); // Signal place is free
                 }
             }
 
@@ -48,10 +50,14 @@ namespace RaceConditions2
                 var writerIndex = 0;
                 for (int i = 0; i < N; i++)
                 {
-                    buffer[writerIndex] = (double)i;
-                    writerReady.Set();
-                    writerIndex = (writerIndex + 1) % BUFFER_SIZE;
-                    readerReady.WaitOne();
+                    empty.WaitOne(); // Wait until place is free
+
+                    lock (buffer)
+                    {
+                        buffer[writerIndex] = i;
+                        writerIndex = (writerIndex + 1) % BUFFER_SIZE;
+                    }
+                    full.Release(); // Signal item available
                 }
             }
         }
